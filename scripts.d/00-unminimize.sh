c4fc3069b482df89b1b681a4afa8c9cba876b637
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2023-04-07 16:48:38 +0000

    fix minminimize issue

--- scripts.d/00-unminimize.sh
+++ scripts.d/00-unminimize.sh
@@ -4,8 +4,11 @@ UNMINIMIZE_PATH=$(which unminimize)
 
 cat $UNMINIMIZE_PATH | \
   sed -e 's/^read/# read/' | \
+  sed -E 's/^( +)(exit 1)$/\1# \2\n\1echo/gi' | \
   sed -e 's/upgrade$/upgrade -y/' > \
   "${UNMINIMIZE_PATH}.new"
 mv $UNMINIMIZE_PATH "${UNMINIMIZE_PATH}.bak"
 mv "${UNMINIMIZE_PATH}.new" $UNMINIMIZE_PATH
+chmod +x $UNMINIMIZE_PATH
+
 unminimize
