56e71b7182bd662a162c3344073d8dfbc334d8a4
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2023-04-07 18:41:49 +0000

    fix

--- scripts.d/25-add-normal-user.sh
+++ scripts.d/25-add-normal-user.sh
@@ -3,5 +3,5 @@
 NEW_USERNAME=${NEW_USERNAME:-torch}
 
 adduser --disabled-password --gecos "" $NEW_USERNAME
-echo "$NEW_USERNAME ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/$NEW_USERNAME
+echo "$NEW_USERNAME ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/$NEW_USERNAME
 visudo -c
